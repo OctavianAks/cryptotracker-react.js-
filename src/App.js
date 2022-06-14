35c2d2208a6d4f9fb51f75509e3047234dfcf448
OctavianAks <107268063+OctavianAks@users.noreply.github.com>
2022-06-14 17:37:30 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Fetch1 from'./components/Fetch1.js';
+
+
+
+
+function App() {
+  return (
+<div className="App">
+<Fetch1/>
+
+
+
+</div>
+  );
+}
+
+export default App;
